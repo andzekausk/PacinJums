0402f72e579cfac2094d9e966e83ffce6985d103
andzekausk <s23kausandz@venta.lv>
2023-12-11 16:02:56 +0200

    Revert "Izveidota parauga scene"
    This reverts commit 38de8aab7db46ffccca0fc91e6cc064631ec3ce8.

--- PacinJums/src/application/TestSceneController.java
+++ PacinJums/src/application/TestSceneController.java
@@ -1,25 +1,5 @@
 package application;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
 public class TestSceneController {
-	@FXML
-	private TextField textField1;
-	@FXML
-	private Label label1;
-	@FXML
-	private Button button1;
 	
-	public void button1Clicked(ActionEvent event) {
-		Stage mainWindow = (Stage) textField1.getScene().getWindow();
-		String title = textField1.getText();
-		mainWindow.setTitle(title);
-		label1.setText(title);
-		button1.setText(title);
-	}
 }
