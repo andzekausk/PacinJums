3f08ddf953c3f0ebe84a071d4d7a3e2dd1b7a14d
andzekausk <s23kausandz@venta.lv>
2023-12-12 21:30:16 +0200

    Izveidots scene change

--- PacinJums/src/module-info.java
+++ PacinJums/src/module-info.java
@@ -1,6 +1,7 @@
 module PacinJums {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens controller to javafx.graphics, javafx.fxml;
