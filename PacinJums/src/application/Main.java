c250f204cacb738b8aee666c3befddd117de20d9
MrAlfonzo <s23mactalfr@venta.lv>
2023-12-13 19:50:40 +0200

    test

--- PacinJums/src/application/Main.java
+++ PacinJums/src/application/Main.java
@@ -23,6 +23,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
-		System.out.println("Sveiki");
+		System.out.println("Ja tu šo lasi, tad tu jau zini kas esi...");
 	}
 }
